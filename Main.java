01a4336c1d88bee5e8aa90f3b39400cb1883dbbd
wrkean <barracaadrianewew@gmail.com>
2025-03-19 09:11:36 +0800

    Edited Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,16 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Problem3.solve();
+        //Problem1.solve();
+        //Problem2.solve();
+        //Problem3.solve();
+        //Problem4.solve();
+        //Problem6.solve();
+        //Problem7.solve();
+        //Problem8.solve();
+        //Problem9.solve();
+        //Problem10.solve();
+
+        System.out.println("");
     }
 }
